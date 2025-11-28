5812ac10e3916dfa8056d3654824e6ac28189b2b
gkv8403 <ghanshyamkalola08@gmail.com>
2025-11-28 12:31:28 +0530

    Assets Creation

---
+++ Assets/Editor/AssetBundleBuilder.cs
@@ -0,0 +1,23 @@
+using UnityEditor;
+using System.IO;
+using UnityEngine;
+
+public class AssetBundleBuilder
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    public static void BuildAllAssetBundles()
+    {
+        string outputPath = "Assets/AssetBundles";
+
+        if (!Directory.Exists(outputPath))
+        {
+            Directory.CreateDirectory(outputPath);
+        }
+
+        BuildPipeline.BuildAssetBundles(outputPath,
+            BuildAssetBundleOptions.None,
+            BuildTarget.StandaloneWindows); // Change as needed (Android/iOS/etc)
+
+        Debug.Log("AssetBundles Built Successfully!");
+    }
+}
